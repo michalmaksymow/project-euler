38a161204bcd8a34fe2a16e5e3047873d6c30664
michalmaksymow <borysbrat@gmail.com>
2021-10-20 11:33:42 +0200

    Update timer documentation

--- cpp/Timer.cpp
+++ cpp/Timer.cpp
@@ -1,7 +1,5 @@
 #include "Timer.h"
 
-/* Explanained in Timer.h */
-
 Timer::Timer()
 {
 	this->start = std::chrono::high_resolution_clock::now();
@@ -12,5 +10,5 @@ Timer::~Timer()
 {
 	this->end = std::chrono::high_resolution_clock::now();
 	this->duration = end - start;
-	std::cout << "This problem took: " << duration.count() << " ms. \n";
+	std::cout << "This problem took: " << duration.count() << " ms to complete. \n";
 }
--- cpp/Timer.h
+++ cpp/Timer.h
@@ -1,18 +1,21 @@
+/*
+ * This timer class is used in every problem that I solved
+ * to measure the efficiency of each problem implementation.
+ *
+ * It is the most basic implementation that I could think of, 
+ * yet it does the job very well. Basically to start this timer 
+ * you only have to initialize it at the beginning of a function 
+ * that you want to measure the duration of. When it is initialized, 
+ * the constructor Timer() is called and it saves a timestamp.
+ * When the function ends the destructor ~Timer() is called, where 
+ * it checks how much time has passed and returns duration in milliseconds.
+*/
+
 #pragma once
+
 #include <chrono>
 #include <iostream>
 
-/*
-	This timer class is used in every problem that I solved
-	in order to measure efficiency of each problem implementation.
-	It is the most basic implementation that I could think of,
-	basically to start this timer you only have to initialize it 
-	at the beginning of a function that you want to measure 
-	duration of. When it is initialized, the constructor Timer() 
-	is called and it sets a point in time, when the funcion ends the
-	destructor ~Timer() is called, where it checks how much time
-	has passed and returns duration in milliseconds.
-*/
 
 class Timer
 {
