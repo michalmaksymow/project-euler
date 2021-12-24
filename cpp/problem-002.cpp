21a7efc07e37747edca234caa21746bc4447f624
michalmaksymow <borysbrat@gmail.com>
2021-12-24 22:06:37 +0100

    Reorganise code, add more explanation comments

--- cpp/problem-002.cpp
+++ cpp/problem-002.cpp
@@ -1,29 +1,27 @@
-#include "Timer.h"
 #include <vector>
+#include "benchmark/timer.hpp"
 
-int main()
-{
-	Timer timer;
+int main() {
+    Timer timer;
 
-	int result {};
-	std::vector<int> sequence{ 1, 2 };
+    int result{};
 
-	/* Filling 'sequence' vector with Fibonacci sequence elements that are lower than 4000000 */
-	do
-	{
-		sequence.push_back(sequence[sequence.size() - 1] + sequence[sequence.size() - 2]);
-	} while (sequence[sequence.size() - 1] + sequence[sequence.size() - 2] < 4000000);
+    // Initializing sequence with two first terms
+    std::vector<int> sequence{1, 2};
 
-	/* Adding together those elements that are even */
-	for (int i = 0; i < sequence.size(); i++)
-	{
-		if (sequence[i] % 2 == 0)
-		{
-			result += sequence[i];
-		}
-	}
+    // Filling sequence with Fibonacci elements that are lower than 4000000
+    do {
+        sequence.push_back(sequence[sequence.size() - 1] + sequence[sequence.size() - 2]);
+    } while (sequence[sequence.size() - 1] + sequence[sequence.size() - 2] < 4000000);
 
-	std::cout << "Problem 2: " << result << std::endl;
+    // Adding together those elements that are even
+    for (int i = 0; i < sequence.size(); ++i) {
+        if (sequence[i] % 2 == 0) {
+            result += sequence[i];
+        }
+    }
 
-	return 0;
+    std::cout << "Problem 2: " << result << std::endl;
+
+    return 0;
 }
