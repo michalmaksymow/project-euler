fa7344fce93236f4de160c0deeb4eb3e12e1036a
michalmaksymow <borysbrat@gmail.com>
2021-12-26 13:09:37 +0100

    Update problem 6 comments

--- cpp/problem-006.cpp
+++ cpp/problem-006.cpp
@@ -6,14 +6,14 @@
 int main() {
     Timer timer;
 
-    // The sum of the squares of the first one hundred natural numbers
-    int sumOfSquares{};
-    // The square of the sum of the first one hundred natural numbers
-    int squareOfSum{};
+    // Holds the sum of the squares of the first one hundred natural numbers
+    uint32_t sumOfSquares{};
+    // Holds the square of the sum of the first one hundred natural numbers
+    uint32_t squareOfSum{};
 
-    for (size_t i = 1; i <= 100; i++) {
+    for (size_t i = 1; i <= 100; ++i) {
         sumOfSquares += pow(i, 2); // Adding squares of i's
-        squareOfSum += i; // Adding i's and then rising the sum to the power of 2
+        squareOfSum += i; // Adding i's together and then rising the sum to the power of 2
     }
 
     std::cout << "Problem 6: " << (squareOfSum * squareOfSum) - sumOfSquares << std::endl;
