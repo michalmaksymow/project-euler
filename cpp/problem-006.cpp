91c711fccb8d8177457abb3b46eb8d91cd0cae4e
michalmaksymow <borysbrat@gmail.com>
2021-12-26 12:10:09 +0100

    Refactor problem 6

--- cpp/problem-006.cpp
+++ cpp/problem-006.cpp
@@ -1,23 +1,20 @@
-#include "Timer.h"
 #include <iostream>
 #include <cmath>
 
-int  main() 
-{
-	Timer timer;
+#include "benchmark/timer.hpp"
 
-	// The sum of the squares of the first one houndred natural numbers
-	int sumOfSquares {};
-	// The square of the sum of the first one houndred natural numbers
-	int squareOfSum {};
+int main() {
+    Timer timer;
 
-	for (int i = 1; i <= 100; i++) 
-	{
-		sumOfSquares += pow(i, 2); // Adding squares of i's
-		squareOfSum += i; // Adding i's and then rising the sum to the power of 2
-	}
+    // The sum of the squares of the first one hundred natural numbers
+    int sumOfSquares{};
+    // The square of the sum of the first one hundred natural numbers
+    int squareOfSum{};
 
-	std::cout << "Problem 6: " << (squareOfSum * squareOfSum) - sumOfSquares << std::endl;
+    for (size_t i = 1; i <= 100; i++) {
+        sumOfSquares += pow(i, 2); // Adding squares of i's
+        squareOfSum += i; // Adding i's and then rising the sum to the power of 2
+    }
 
-	return 0;
+    std::cout << "Problem 6: " << (squareOfSum * squareOfSum) - sumOfSquares << std::endl;
 }
