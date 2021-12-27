0b75e397ab2f7bd08b45d35bb0cf15d5c64efde6
michalmaksymow <borysbrat@gmail.com>
2021-12-27 10:57:24 +0100

    Refactor problem 9, update run script

--- cpp/problem-009.cpp
+++ cpp/problem-009.cpp
@@ -1,27 +1,23 @@
-#include "Timer.h"
 #include <iostream>
 #include <cmath>
 
-int main() 
-{
-	Timer startTimer;
+#include "benchmark/timer.hpp"
 
-	// a < b < c
-	for (int a = 1; a < 1000; a++) 
-	{
-		// Making sure that b is bigger than a by at least 1
-		for (int b = a + 1; b < 1000; b++) 
-		{
-			// a + b + c = 1000 => c = 1000 - a - b
-			int c = 1000 - a - b;
-			// Conditions for a Pythagorean triplet
-			if (pow(a, 2) + pow(b, 2) == pow(c, 2) && b < c) 
-			{
-				// Returning the product abc
-				std::cout << "Problem 9: " << (a * b * c) << std::endl;
-			}
-		}
-	}
+int main() {
+    Timer timer;
 
-	return 0;
+    // Brute force method
+    for (int a = 1; a < 1000; a++) {
+        // Make sure that b is bigger than a by at least 1
+        for (int b = a + 1; b < 1000; b++) {
+            // Here a + b + c = 1000, so c = 1000 - a - b
+            int c = 1000 - a - b;
+            // Conditions for a Pythagorean triplet
+            if (pow(a, 2) + pow(b, 2) == pow(c, 2) && b < c) {
+                // If the condition is met, return the product
+                std::cout << "Problem 9: " << (a * b * c) << std::endl;
+                return 0;
+            }
+        }
+    }
 }
