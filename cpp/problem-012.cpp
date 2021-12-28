136188a70c215988839af873a268bae717690b75
michalmaksymow <borysbrat@gmail.com>
2021-12-28 18:21:00 +0100

    Refactor problem 12, add comments

--- cpp/problem-012.cpp
+++ cpp/problem-012.cpp
@@ -1,38 +1,38 @@
-#include "Timer.h"
 #include <iostream>
 #include <cmath>
 
-long triangleNumber(int n) 
-{
-	long number = 0;
-	for (long i = 1; i <= n; i++) 
-	{
-		number += i;
-	}
-	return number;
+#include "benchmark/timer.hpp"
+
+// Computes nth triangle number by summing up numbers from 1 to n together
+uint32_t computeTriangleNumber(uint32_t n) {
+    uint32_t number = 0;
+    for (size_t i = 1; i <= n; ++i)
+        number += i;
+
+    return number;
 }
 
-int main() 
-{
-	Timer startTimer;
+int main() {
+    Timer timer;
 
-	int n = 1; // Index of checked triangle number
-	while (true) 
-	{
-		long checkedNumber = triangleNumber(n);
-		int divisors = 0;
-		for (int i = 1; i <= (int)sqrt(checkedNumber); i++) 
-		{
-			if (checkedNumber % i == 0)
-				divisors += 2;
-		}
-		if (divisors > 500) 
-		{
-			std::cout << "Problem 12: " << (checkedNumber) << std::endl;
-			break;
-		}
-		n++;
-	}
+    // Holds index of checked triangle number
+    uint32_t n = 1;
 
-	return 0;
+    // Iterate until wanted triangle number is found
+    while (++n) {
+        auto checkedNumber = computeTriangleNumber(n);
+
+        auto divisors = 0u;
+
+        // Compute number of divisors of currently checked triangle number
+        for (size_t i = 1; i * i <= checkedNumber; ++i) {
+            if (checkedNumber % i == 0)
+                divisors += 2;
+        }
+
+        if (divisors > 500) {
+            std::cout << "Problem 12: " << checkedNumber << std::endl;
+            break;
+        }
+    }
 }
