4af36d4738f7a5790ccc93467c10fa92416f80fe
michalmaksymow <borysbrat@gmail.com>
2021-12-29 16:12:09 +0100

    Refactor problem 14

--- cpp/problem-014.cpp
+++ cpp/problem-014.cpp
@@ -1,39 +1,34 @@
-#include "Timer.h"
 #include <iostream>
 
-int main() 
-{
-	Timer startTimer;
+#include "benchmark/timer.hpp"
 
-	long long current = 0;
-	long maxTerms = 0, maxIndex = 0;
-	int terms = 0;
+int main() {
+    Timer timer;
 
-	for (long i = 2; i < 1000000; i++) 
-	{
-		current = i;
-		terms = 1;
-		while (current != 1) 
-		{
-			if (current % 2 == 0) 
-			{
-				current = current / 2;
-				terms++;
-			}
-			else 
-			{
-				current = (3 * current) + 1;
-				terms++;
-			}
-		}
-		if (terms > maxTerms) 
-		{
-			maxTerms = terms;
-			maxIndex = i;
-		}
-	}
+    // Holds number of terms and number
+    uint32_t maxTerms{0}, maxIndex{0};
 
-	std::cout << "Problem 14: " << maxIndex << std::endl;
+    // Finding which number under one million has the largest sequence - simple brute force method
+    for (size_t i = 2; i < 1000000; ++i) {
+        uint64_t current{i};
+        uint32_t terms{1};
 
-	return 0;
+        // Computing Collatz sequence terms for currently considered number
+        while (current != 1) {
+            if (current % 2 == 0)
+                current = current / 2;
+            else
+                current = (3 * current) + 1;
+
+            terms++;
+        }
+
+        // Checking if current one has the most terms
+        if (terms > maxTerms) {
+            maxTerms = terms;
+            maxIndex = i;
+        }
+    }
+
+    std::cout << "Problem 14: " << maxIndex << std::endl;
 }
