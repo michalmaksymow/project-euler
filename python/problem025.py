2c0319ec7fdec66e5829d62fcaf4225edd9a61e0
michalmaksymow <borysbrat@gmail.com>
2022-12-03 17:40:50 +0100

    Fix formatting in .py files

--- python/problem025.py
+++ python/problem025.py
@@ -1,16 +1,18 @@
 from timer import Timer
 
+
 def compute():
-    t = Timer()
+    Timer()
 
     f1 = 1
     f2 = 1
     term = 2
 
-    while (len(str(f2)) < 1000):
+    while len(str(f2)) < 1000:
         f1, f2 = f2, f1 + f2
         term += 1
-        
+
     print(term)
+
 
 compute()
--- python/timer.py
+++ python/timer.py
@@ -1,10 +1,10 @@
 import time
 
+
 class Timer(object):
 
     def __init__(self):
         self.startTime = time.time()
 
     def __del__(self):
-        print("Elapsed time: " + str((time.time() - self.startTime)*1000) + "ms")
-    
+        print("Elapsed time: " + str((time.time() - self.startTime) * 1000) + "ms")
