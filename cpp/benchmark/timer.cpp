96dce7ac3edbc250964e7a776e16294e72ed24e4
Michał Maksymów <44416895+michalmaksymow@users.noreply.github.com>
2023-10-01 11:44:02 +0000

    fix: Linting

--- cpp/benchmark/timer.cpp
+++ cpp/benchmark/timer.cpp
@@ -1,14 +1,12 @@
 #include "timer.hpp"
 
-Timer::Timer()
-{
-	this->start = std::chrono::high_resolution_clock::now();
+Timer::Timer() {
+    this->start = std::chrono::high_resolution_clock::now();
     this->duration = std::chrono::duration<float, std::milli>::zero();
 }
 
-Timer::~Timer()
-{
-	this->end = std::chrono::high_resolution_clock::now();
+Timer::~Timer() {
+    this->end = std::chrono::high_resolution_clock::now();
     this->duration = end - start;
-	std::cout << "This problem took: " << duration.count() << " ms to complete. \n";
+    std::cout << "This problem took: " << duration.count() << " ms to complete. \n";
 }
--- cpp/benchmark/timer.hpp
+++ cpp/benchmark/timer.hpp
@@ -1,28 +1,16 @@
-/*
- * This timer class is used in every problem that I solved
- * to measure the efficiency of each problem implementation.
- *
- * It is the most basic implementation that I could think of, 
- * yet it does the job very well. Basically to start this timer 
- * you only have to initialize it at the beginning of a function 
- * that you want to measure the duration of. When it is initialized, 
- * the constructor Timer() is called, and it saves a timestamp.
- * When the function ends the destructor ~Timer() is called, where 
- * it checks how much time has passed and returns duration in milliseconds.
-*/
-
-#pragma once
+#ifndef TIMER_H
+#define TIMER_H
 
 #include <chrono>
 #include <iostream>
 
-class Timer
-{
+class Timer {
 private:
     std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
-	std::chrono::duration<float, std::milli> duration;
-
+    std::chrono::duration<float, std::milli> duration;
 public:
     Timer();
-	~Timer();
+    ~Timer();
 };
+
+#endif
