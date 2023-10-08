e357fb4eedbc8dcb48bd59467e2787d02d60a314
Michał Maksymów <44416895+michalmaksymow@users.noreply.github.com>
2023-10-08 11:03:50 +0000

    Include algorithm in problem 3

--- cpp/problem-003.cpp
+++ cpp/problem-003.cpp
@@ -1,12 +1,11 @@
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 #include "benchmark/timer.hpp"
 
-const uint64_t THE_NUMBER = 600851475143LL;
-
-std::vector <uint32_t> findPrimeFactors(uint64_t number) {
-    std::vector <uint32_t> factors;
+std::vector<uint32_t> findPrimeFactors(uint64_t number) {
+    std::vector<uint32_t> factors;
 
     for (size_t i = 2; i <= sqrt(number); ++i) {
         while (number % i == 0) {
@@ -21,14 +20,17 @@ std::vector <uint32_t> findPrimeFactors(uint64_t number) {
     return factors;
 }
 
-int main() {
-    Timer timer;
+void solve() {
+    uint64_t THE_NUMBER = 600851475143LL;
 
-    // Finding prime factors of the number defined in the task
     auto primeFactors = findPrimeFactors(THE_NUMBER);
 
-    // Finding maximum value in primeFactors vector
     auto maxElement = std::max_element(primeFactors.begin(), primeFactors.end());
 
     std::cout << "Problem 3: " << *maxElement << std::endl;
+}
+
+int main() {
+    Timer timer;
+    solve();
 }
