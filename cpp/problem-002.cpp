e07a9ae5909ca4b54ad8c054c73e359c7782b1c8
Michał Maksymów <44416895+michalmaksymow@users.noreply.github.com>
2023-10-08 11:00:57 +0000

    Fix types in problem 2

--- cpp/problem-002.cpp
+++ cpp/problem-002.cpp
@@ -2,9 +2,7 @@
 
 #include "benchmark/timer.hpp"
 
-int main() {
-    Timer timer;
-
+void solve() {
     int result{};
 
     // Initializing sequence with two first terms
@@ -16,11 +14,16 @@ int main() {
     } while (sequence[sequence.size() - 1] + sequence[sequence.size() - 2] < 4000000);
 
     // Adding together those elements that are even
-    for (int i = 0; i < sequence.size(); ++i) {
+    for (uint32_t i{0}; i < sequence.size(); ++i) {
         if (sequence[i] % 2 == 0) {
             result += sequence[i];
         }
     }
 
     std::cout << "Problem 2: " << result << std::endl;
+}
+
+int main() {
+    Timer timer;
+    solve();
 }
